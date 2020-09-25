738040cbb0e51f12ac0670a3d2d278fa11cf0d88
jacksons396 <71662803+jacksons396@users.noreply.github.com>
2020-09-25 14:25:52 -0400

    space whatevers. avoid the whatevers at all costs

--- main.ts
+++ main.ts
@@ -5,25 +5,49 @@ input.onButtonPressed(Button.B, function () {
     sprite.change(LedSpriteProperty.X, 1)
 })
 let sprite: game.LedSprite = null
+let Gound = 5
 game.setLife(3)
 let Lives = 3
 sprite = game.createSprite(2, 5)
 let Rock = game.createSprite(randint(1, 5), 0)
+let Rock_2 = game.createSprite(randint(1, 5), 0)
 basic.forever(function () {
     if (Lives == 0) {
         sprite.delete()
         basic.showString("Game over")
     }
-    if (sprite.isTouching(Rock)) {
-        Lives += -1
+    if (Rock.isTouching(sprite)) {
+        sprite.delete()
+        basic.showString("Game over")
     }
 })
 basic.forever(function () {
-    if (Rock.get(LedSpriteProperty.Y) == "5") {
+    if (Rock.get(LedSpriteProperty.Y) == sprite.get(LedSpriteProperty.Y)) {
         Rock.set(LedSpriteProperty.Y, 0)
+        Rock.set(LedSpriteProperty.X, randint(1, 5))
+    }
+})
+basic.forever(function () {
+    if (Rock_2.get(LedSpriteProperty.Y) == sprite.get(LedSpriteProperty.Y)) {
+        Rock_2.set(LedSpriteProperty.Y, 0)
+        Rock_2.set(LedSpriteProperty.X, randint(1, 5))
     }
 })
 basic.forever(function () {
     Rock.change(LedSpriteProperty.Y, 1)
-    basic.pause(500)
+    basic.pause(randint(250, 350))
+})
+basic.forever(function () {
+    Rock_2.change(LedSpriteProperty.Y, 1)
+    basic.pause(randint(250, 350))
+})
+basic.forever(function () {
+    if (Lives == 0) {
+        sprite.delete()
+        basic.showString("Game over")
+    }
+    if (sprite.isTouching(Rock_2)) {
+        sprite.delete()
+        basic.showString("Game over")
+    }
 })
